7cba82b70234c9b6f41a227f3d29121a84f18f2f
sairajsk777 <87169255+sairajsk777@users.noreply.github.com>
2021-07-25 10:54:04 +0530

    Add files via upload

---
+++ eventspage/eventsjs.js
@@ -0,0 +1,8 @@
+function op() {
+    // console.log("HappeningOP");
+    document.querySelector(".navcont").style.transform = "translateX(0)";
+}
+function closethis() {
+    // console.log("HappeningCL");
+    document.querySelector(".navcont").style.transform = "translateX(-150%)";
+}
